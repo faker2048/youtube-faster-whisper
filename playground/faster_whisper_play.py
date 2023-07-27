1495c4579c9b9f8bf406bff1e49a0c196651d032
faker <nspyia2002@gmail.com>
2023-07-27 12:48:01 +0800

    precommit test

--- playground/faster_whisper_play.py
+++ playground/faster_whisper_play.py
@@ -4,17 +4,17 @@ from faster_whisper import WhisperModel
 
 
 def main():
-    start_init = time.time()  # 开始初始化的时间
+    start_init = time.time()
     model = WhisperModel("tiny.en", device="cuda", compute_type="float16")
-    end_init = time.time()  # 结束初始化的时间
+    end_init = time.time()
     print(f"Initialization took {end_init - start_init} seconds")
 
-    start_inference = time.time()  # 开始推理的时间
+    start_inference = time.time()
     segments, info = model.transcribe("data/jfk.flac", "en", beam_size=5)
 
     for segment in segments:
         print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
-    end_inference = time.time()  # 结束推理的时间
+    end_inference = time.time()
     print(f"Inference took {end_inference - start_inference} seconds")
 
 
