7bd5b37adc7784a2aba37f3d4267cbd4bcee242e
faker2048 <nspyia2002@gmail.com>
2023-07-29 15:02:18 +0800

    ujpdate transcribe.py, download.py

--- src/whisper/transcribe.py
+++ src/whisper/transcribe.py
@@ -2,12 +2,17 @@ from loguru import logger
 from faster_whisper import WhisperModel
 
 
-def initialize_whisper_model(model_name: str = "tiny.en"):
+def initialize_whisper_model(
+    model_name: str = "tiny.en", model_root: str | None = None
+):
+    logger.info(f"Initializing WhisperModel with model {model_name}.")
     try:
-        return WhisperModel(model_name, device="cuda", compute_type="float16")
+        return WhisperModel(
+            model_name, device="cuda", compute_type="float16", download_root=model_root
+        )
     except RuntimeError:
         logger.info(
             "WhisperModel could not be initialized with CUDA device.\
                   Falling back to CPU device."
         )
-        return WhisperModel(model_name, device="cpu")
+        return WhisperModel(model_name, device="cpu", download_root=model_root)
--- src/youtube/download.py
+++ src/youtube/download.py
@@ -1,38 +1,12 @@
 import yt_dlp
 
 
-def __best_format_selector(ctx):
-    """Select the best video and the best audio that won't result in an mkv.
-    NOTE: This is just an example and does not handle all cases"""
-
-    # formats are already sorted worst to best
-    formats = ctx.get("formats")[::-1]
-
-    # acodec='none' means there is no audio
-    best_video = next(
-        f for f in formats if f["vcodec"] != "none" and f["acodec"] == "none"
-    )
-
-    # find compatible audio extension
-    audio_ext = {"mp4": "m4a", "webm": "webm"}[best_video["ext"]]
-    # vcodec='none' means there is no video
-    best_audio = next(
-        f
-        for f in formats
-        if (f["acodec"] != "none" and f["vcodec"] == "none" and f["ext"] == audio_ext)
-    )
-
-    # These are the minimum required fields for a merged format
-    yield {
-        "format_id": f'{best_video["format_id"]}+{best_audio["format_id"]}',
-        "ext": best_video["ext"],
-        "requested_formats": [best_video, best_audio],
-        # Must be + separated list of protocols
-        "protocol": f'{best_video["protocol"]}+{best_audio["protocol"]}',
-    }
-
-
-def download(url: str, threads: int = 8) -> str:
+# audio only format: "worstaudio[tbr>100]/bestaudio/best"
+# video only format: "bestvideo[height>=1080]/bestvideo"
+# video and audio format: "bestvideo+bestaudio/best"
+def download(
+    url: str, threads: int = 8, format: str = "bestvideo+bestaudio/best"
+) -> str:
     """Download a video from YouTube and return the file name."""
     file_name = ""
 
@@ -42,7 +16,7 @@ def download(url: str, threads: int = 8) -> str:
             file_name = d["filename"]
 
     ydl_opts = {
-        "format": __best_format_selector,
+        "format": format,
         "concurrent_fragment_downloads": threads,
         "progress_hooks": [file_name_hook],
         "retries": 10,
@@ -56,25 +30,7 @@ def download(url: str, threads: int = 8) -> str:
     return file_name
 
 
-def download_audio(url: str, threads: int = 8) -> str:
-    """Download an audio from YouTube and return the file name."""
-    file_name = ""
-
-    def file_name_hook(d):
-        nonlocal file_name
-        if d["status"] == "finished":
-            file_name = d["filename"]
-
-    ydl_opts = {
-        "format": "worstaudio[tbr>100]/bestaudio/best",
-        "concurrent_fragment_downloads": 8,
-        "progress_hooks": [file_name_hook],
-        "retries": 10,
-        "file_access_retries": 10,
-        "fragment_retries": 10,
-    }
-
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-
-    return file_name
+def download_audio(
+    url: str, threads: int = 8, format: str = "worstaudio[tbr>100]/bestaudio/best"
+) -> str:
+    return download(url, threads, format)
