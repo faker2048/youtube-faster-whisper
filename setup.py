0b1b7d38dd6d3a65aa56474149865af32871fb00
faker2048 <nspyia2002@gmail.com>
2023-11-13 22:24:29 +0800

    chore: update to 0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ytws",
-    version="0.2.1",
+    version="0.2.2",
     description="YouTube Whisper - A YouTube Downloader with Transcription",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -15,9 +15,9 @@ setup(
     packages=find_packages(include=["src", "src.*"]),
     install_requires=[
         "click==8.1.6",
-        "faster_whisper==0.7.1",
+        "faster_whisper==0.9.0",
         "loguru==0.7.0",
-        "yt_dlp==2023.9.24",
+        "yt_dlp==2023.10.13",
     ],
     entry_points="""
         [console_scripts]
