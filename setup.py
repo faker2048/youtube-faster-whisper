dee3d91e8ec5d13ba63bd5dee8028064248fa7b3
faker <nspyia2002@gmail.com>
2023-11-29 23:49:06 +0800

    chore: update to 0.4.1 (add interrupt support for download thread with Ctrl+C)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ytws",
-    version="0.4.0",
+    version="0.4.1",
     description="YouTube Whisper - A YouTube Downloader with Transcription",
     long_description=long_description,
     long_description_content_type="text/markdown",
